5fa8d9525174bfc7410b1fb52f92e4d096b1d1ff
dogj <924625141@qq.com>
2018-04-13 00:19:02 +1200

    toolbar and drawer
    toolbar and drawer
    image fix

--- app/src/main/java/com/example/administrator/boostertest/MainActivity.java
+++ app/src/main/java/com/example/administrator/boostertest/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
@@ -17,16 +19,13 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.inflateMenu(R.menu.main);
-        toolbar.setTitle("Simplify");
-        toolbar.setTitleMarginStart(100);
         setSupportActionBar(toolbar);
+//        toolbar.inflateMenu(R.menu.main);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -35,22 +34,29 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.defensive) {
             Toast.makeText(MainActivity.this,"haha",Toast.LENGTH_SHORT);
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.conservative) {
             Toast.makeText(MainActivity.this,"waha",Toast.LENGTH_SHORT);
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.balanced) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.balanced_growth) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.growth) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.aggressive_growth) {
 
         }
 
 
         return true;
     }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
 }
