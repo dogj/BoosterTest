ae96af6493462f890b32202f311750fb61109ca1
dogj <924625141@qq.com>
2018-04-14 12:04:19 +1200

    Introduction page
    Introduction page with banner

--- app/src/main/java/com/example/administrator/boostertest/ContentFragment.java
+++ app/src/main/java/com/example/administrator/boostertest/ContentFragment.java
@@ -34,7 +34,7 @@ public class ContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_defensive,container,false);
+        View view = inflater.inflate(R.layout.activity_introduction,container,false);
 
         return view;
     }
